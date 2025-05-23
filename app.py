edcb6aeb1e185714511ac0dcb10bd26c2f1c191e
Muhammad Abdullah <39337161+abdullah249@users.noreply.github.com>
2025-05-23 22:07:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,5 @@ def download_file(filename):
         return redirect(url_for('upload_file'))
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port) 
